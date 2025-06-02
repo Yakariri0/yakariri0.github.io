c559eed4e3ac2c436c1c7f135f7badf4edadb332
Yakariri0 <137888622+Yakariri0@users.noreply.github.com>
2025-06-02 20:19:25 +0300

    Add files via upload

---
+++ scroll.js
@@ -0,0 +1,21 @@
+
+window.addEventListener('scroll', reveal);
+function reveal() {
+  var reveals = document.querySelectorAll('.reveal');
+  for (var i = 0; i < reveals.length; i++) {
+    var element = reveals[i];
+    if (element.closest('#testimonials') || element.closest('#contact') || element.closest('footer')) {
+      element.classList.add('active');
+      continue;
+    }
+    var windowHeight = window.innerHeight;
+    var elementTop = element.getBoundingClientRect().top;
+    var elementVisible = 100;
+    if (elementTop < windowHeight - elementVisible) {
+      element.classList.add('active');
+    } else {
+      element.classList.remove('active');
+    }
+  }
+}
+reveal(); // إظهار العناصر مباشرة عند التحميل
